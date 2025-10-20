54d72dc8daaf96197d7f8255b17fc0a86a08c285
kskue777-alt <kskue777@gmail.com>
2025-10-20 11:05:23 +0900

    Fix widget test by providing push to talk controller

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
 import 'package:tnt/main.dart';
 
 void main() {
   testWidgets('renders push to talk tab', (WidgetTester tester) async {
     await tester.pumpWidget(
-      const MaterialApp(
-        home: HomeScreen(),
+      ChangeNotifierProvider(
+        create: (_) => PushToTalkController(),
+        child: const MaterialApp(
+          home: HomeScreen(),
+        ),
       ),
     );
 
